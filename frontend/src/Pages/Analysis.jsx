44267dc38b38dc4aa4bc4fbd85ec6f35b5020361
Shreevathsa05 <144523306+Shreevathsa05@users.noreply.github.com>
2025-10-04 17:40:56 +0530

    Update Analysis.jsx

--- frontend/src/Pages/Analysis.jsx
+++ frontend/src/Pages/Analysis.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { useExam } from "../components/ExamContext";
+import { useExam } from "../Components/ExamContext";
 
 export default function Analysis() {
     const backendurl=`https://hackwave-2025.onrender.com`
