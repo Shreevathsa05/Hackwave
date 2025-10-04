10e5c0aca074ca306fed89b55ee86fea90733b6a
Shreevathsa05 <144523306+Shreevathsa05@users.noreply.github.com>
2025-10-04 17:41:26 +0530

    Update Quiz.jsx

--- frontend/src/Pages/Quiz.jsx
+++ frontend/src/Pages/Quiz.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { useExam } from "../components/ExamContext";
+import { useExam } from "../Components/ExamContext";
 
 export default function Quiz() {
     const backendurl=`https://hackwave-2025.onrender.com`
